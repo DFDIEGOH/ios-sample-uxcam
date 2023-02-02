d78a6ed266dacb8dedfa11f0eeeb0b8c4fed2830
Diego Hernandez <diego@uxcam.com>
2023-02-02 12:15:22 +0100

    UXCam integration on sample project

---
+++ Splitter/AppDelegate.swift
@@ -0,0 +1,56 @@
+//
+//  AppDelegate.swift
+//  Splito
+//
+//  Created by Aleksandr AMorozov on 04.12.2021.
+//
+
+import UIKit
+import IQKeyboardManagerSwift
+//import UXCam
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    //MARK: - Portrait orientation
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
+        sleep(1)
+//      let config = UXCamConfiguration(appKey: "2q3raqi4op4pkdo")
+//
+//      UXCam.optIntoSchematicRecordings()
+//      UXCam.start(with: config) {(status) in
+//        if (status) {
+//          let url = UXCam.urlForCurrentSession()
+//          UXCam.logEvent(url ?? "no url")
+//        }
+//      }
+
+
+        
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    
+}
+
